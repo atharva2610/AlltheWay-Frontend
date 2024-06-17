42c422511c5d7dfb4b2e861c94ce3ea79808fb94
atharva2610 <atharvavishwakarma526@gmail.com>
2024-06-17 20:30:12 +0530

    update host

--- src/configrations/backendConfig.js
+++ src/configrations/backendConfig.js
@@ -1,4 +1,4 @@
-export const baseURL = "http://"+import.meta.env.VITE_HOST;
-export const baseSocketURL = "ws://"+import.meta.env.VITE_HOST;
+export const baseURL = "https://"+import.meta.env.VITE_HOST;
+export const baseSocketURL = "wss://"+import.meta.env.VITE_HOST;
 export const rzId = import.meta.env.VITE_RZ_PUBLIC_KEY;
 export const rzSecretKey = import.meta.env.VITE_RZ_SECRET_KEY;
